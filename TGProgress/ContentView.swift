7559203a4fca4eecaa1eeca21cf7a2800c8de0e0
satish.v <satish.v@canopas.com>
2021-01-06 18:19:52 +0530

    Project init

--- TGProgress/ContentView.swift
+++ TGProgress/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    let segmentCount = 200
+    let colors :[Color]
+    
+    @State private var xOffset:CGFloat = 0
+    
+    @State private var resetValue:CGFloat = 320*2
+    @State private var timer:Timer? = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            VStack {
+                ZStack {
+                    LinearGradient(gradient: Gradient(colors: colors), startPoint: .leading, endPoint: .trailing)
+                        .frame(width: geo.frame(in: CoordinateSpace.local).width - 32,  height: 10)
+                        .cornerRadius(5.0)
+                    HStack() {
+                        ForEach(0..<segmentCount) {_ in
+                            Spacer()
+                            Rectangle().foregroundColor(.white)
+                                .rotationEffect(.init(degrees: 44))
+                                .frame(width: 11, height: 47)
+                            Spacer()
+                        }
+                        .frame(height: 10)
+                        .offset(x: xOffset)
+                    }
+                    .frame(width: geo.frame(in: CoordinateSpace.local).width * 2)
+                }
+                .frame(width: geo.frame(in: CoordinateSpace.local).width - 32)
+                .clipped()
+            
+            }
+            .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
+            .onAppear(perform: {
+                self.resetValue = (geo.frame(in: CoordinateSpace.local).width * 2)
+                xOffset = -resetValue
+                timer = statAnimation()
+            })
+        }
+    }
+    
+    @discardableResult
+    func statAnimation() -> Timer {
+        func update(_ : Timer) {
+            if self.xOffset > 0 {
+                self.xOffset = -resetValue
+            } else {
+                self.xOffset += 1
+            }
+        }
+        
+       return Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: update(_:))
+    }
+    
+    func stopAnimation() {
+        timer?.invalidate()
+        timer = nil
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(colors: [Color.blue, Color.green])
     }
 }
+
