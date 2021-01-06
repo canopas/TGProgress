6c2486c1432667afc40c27086e1117596cca8da8
satish.v <satish.v@canopas.com>
2021-01-06 18:08:03 +0530

    Initial Commit

---
+++ TGProgress/TGProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  TGProgressApp.swift
+//  TGProgress
+//
+//  Created by satish.v on 06/01/21.
+//
+
+import SwiftUI
+
+@main
+struct TGProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
